90c0038cf9bd6d35f76954a5116a8f2fccadf506
Yura <merda@tut.by>
2022-02-15 03:15:51 +0300

    Redux Toolkit: configureStore()

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import filters from '../reducers/filters';
 import heroes from '../reducers/heroes';
-import ReduxThunk from 'redux-thunk'
+import { configureStore } from '@reduxjs/toolkit';
 
 const stringMiddleware = ({dispatch, getState}) => (next) => (action) => {
      if(typeof action === 'string') {
@@ -28,18 +27,21 @@ const enhancer = (createStore) => (...args) => {
      }  
      return store;
 }
-
+/* 
 const store = createStore
      (combineReducers({heroes: heroes, filters: filters}), 
-     //склеем несколько функций для добавления 2ым аргументом
-  /*    compose(
-          enhancer,
-          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-     ) */
-     //
      compose(
           applyMiddleware(stringMiddleware, ReduxThunk), 
           window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-     );
+     ); */
+
+const store = configureStore({
+     //редьюсеры через сокращенный синтексиз
+     reducer: {heroes, filters},
+     //добавим все мидлвары тулкита а также наш собственный мидлвар через метод канкат
+     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+     //условие включения девтулса
+     devTools: process.env.NODE_ENV !=='production', 
+})
 
 export default store;
