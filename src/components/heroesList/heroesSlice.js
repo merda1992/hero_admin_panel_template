136e81afe66aced97da7c8418131913ecfefabdd
Yura <merda@tut.by>
2022-02-18 00:54:18 +0300

    createAsyncThunk() - heroes

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,10 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {useHttp} from '../../hooks/http.hook';
 
 //осздание начального состояния
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle'
 }
+
+//возвращает аж 3 экшн криэтора - (пэдинг фулфилд и реджектед)
+export const fetchHeroes = createAsyncThunk(
+// тип дейсвия в формате имени среза (откуда он берется - путь)
+    'heroes/fetchHeroes',
+//  функция которая должная вернуть промис - т е асинхронный код(у нее есть два принимающих аргумента - посмотреть их в документации)
+    () => {
+        const {request} = useHttp();
+       return request("http://localhost:3001/heroes")
+    }
+);
 
 const heroesSlice = createSlice({
     // имя нашего среза (пространсвто имен)
@@ -14,21 +26,26 @@ const heroesSlice = createSlice({
     //формирование обьектов с редьюсерами
     reducers: {
         //экшн криеторы и те деййтвия которые будут под них подвязыватсья
-        heroesFetching: state => {state.heroesLoadingStatus = 'loading'},
-        heroesFetched: (state, action) => {
-            state.heroesLoadingStatus = 'idle';
-            state.heroes = action.payload;
-        },
-        heroesFetchingError: state => {
-            state.heroesLoadingStatus = 'error';
-        },
         heroCreated: (state, action) => {
             state.heroes.push(action.payload);
         },
         heroDeleted: (state, action) => {
             state.heroes = state.heroes.filter(item => item.id !== action.payload);
         }
-    } 
+    },
+    //закинем сторонние редьюсеры коими являются fetchHeroes
+    extraReducers: (builder) => {
+        builder 
+            .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
+            .addCase(fetchHeroes.fulfilled, (state, action) => {
+                state.heroesLoadingStatus = 'idle';
+                state.heroes = action.payload;
+            })
+            .addCase(fetchHeroes.rejected, (state, action) => {
+                state.heroes.push(action.payload);
+            })
+            .addDefaultCase (() => {});
+    }
 });
 
 //достанем из нашей сущности экшены и редьюсеры
